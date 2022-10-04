f360a865c4a4721e1f4ae9cd5c8532ca0d3d4bf9
Mudassir-Ahmed-Khan <mudassirahmed395@gmail.com>
2022-10-05 01:57:54 +0500

    Update url and templates

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,19 +1,15 @@
 from django.urls import path
 from todolist import views
 import debug_toolbar
-
 import todolist
-
 app_name = 'todolist'
+
 urlpatterns = [
+    path("", views.display_form, name = 'display_form'),
     path('hello/', views.home),
-    path("", views.add_person),
-    path("add_task/", views.add_task),
-    path('description/', views.description),
-    path('description/<int:number>/', views.description_number, name = 'description'),
-    path('users/', views.users),
-    path('task/<int:number>/', views.display_task, name = 'display_task'),
-    path('<int:id>/delete/', views.delete_task, name = 'delete_task'),
+    path('display_task/', views.display_task, name = 'display_task'),
+    path("delete_task/<task_id>/", views.delete_task, name = 'delete_task'),
+    path("add/", views.add, name = 'add'),
 ]
 
 
--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,9 @@
 from multiprocessing import context
+from django.contrib import messages
 from django.template import loader
 from .forms import PersonDetailsFrom, TaskDetailsFrom
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.utils import timezone
 from .models import Person, Task
 from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
@@ -13,61 +14,52 @@ from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
 def home(request):
     return HttpResponse("Django!")
 
-def description(request):
-    i=3
-    i=i+1
-    return HttpResponse("I am django i am a very good website development framework %s" %i)
+
+def users(request):
+    users_set = list(Person.objects.all())
+    return render(request, 'todolist/index.html', {'users_set': users_set})
 
 
-def description_number(request, number):
-
-    return HttpResponse("My description number is %s" % number)
-
-
-
-
-def add_person(request):
+def add_user(request):
     context ={}
     form = PersonDetailsFrom(request.POST or None)
     if form.is_valid():
         form.save()
-    
+        # return HttpResponseRedirect("/")
     context['form']= form
-    return render(request, "todolist/person_details_form.html", context)
+    return render(request, "todolist/index.html", context)
 
 
 
-def add_task(request):
-    context = {}
+
+
+def display_task(request):
+    task_per_person = list(Task.objects.filter(person_id = 1))
+    user_name = Person.objects.get(pk = 1)
     task_details_form = TaskDetailsFrom(request.POST or None)
     if task_details_form.is_valid():
         task_details_form.save()
+        return HttpResponseRedirect("/display_task/")
+    data_to_display = {'task_per_person':task_per_person, 'user_name':user_name, 'task_details_form':task_details_form}
+    return render(request, 'todolist/index.html',data_to_display)
 
-    context['task_details_form'] = task_details_form
-    return render(request, "todolist/task_details_form.html", context)
-
-def delete_task(request, id):
-    context = {}
-    obj = get_object_or_404(Task, id = id)
-    if request.method == "POST":
-        obj.delete()
-        return HttpResponseRedirect("/task/<int:id>/")
-    return render(request, "todolist/delete_tasks.html", context)
-
+def delete_task(request, task_id):
+    obj = Task.objects.get(id=task_id)
+    # if request.method == "POST":
+    obj.delete()
+    # messages.info("item removed !!!")
+    return HttpResponseRedirect("/display_task/")
 
 
-
-def users(request):
-    users_set = list(Person.objects.all())
-    # try:
-    #     users_set = list(Person.objects.all())
-    # except Person.DoesNotExist:
-    #     raise Http404("Person is not available")
-    return render(request, 'todolist/index.html', {'users_set': users_set})
+def display_form(request):
+    return render(request, 'todolist/add.html')
 
 
-def display_task(request, number):
-    context = {}
-    task_per_person = Task.objects.filter(person_id = number)
-    return render(request, "todolist/tasks_per_person.html", {'task_per_person':task_per_person})
+def add(request):
+    val1 = int(request.POST["number1"])
+    val2 = int(request.POST["number2"])
+    result = val1+val2
+    return render(request, 'todolist/add.html', {'result': result})
+
+
 
